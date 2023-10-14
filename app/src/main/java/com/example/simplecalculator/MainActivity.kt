533c5aeb80b57392cb30316c206299a4298467dc
Okello Sam <samuelokello929@gmail.com>
2023-10-14 21:56:46 +0300

    Refactor

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.simplecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
+import androidx.activity.viewModels
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,24 +15,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.simplecalculator.ui.theme.SimpleCalculatorTheme
 
@@ -46,82 +40,70 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    SimpleCalculatorLayout()
+                    val viewModel: MainViewModel by viewModels()
+                    Column(
+                        modifier = Modifier.padding(8.dp).verticalScroll(rememberScrollState()),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = stringResource(R.string.calculate_sum),
+                            modifier = Modifier.padding(bottom = 16.dp)
+                                .align(alignment = Alignment.Start),
+                        )
+
+                        EditNumberField(
+                            label = R.string.first_number,
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next,
+                            ),
+                            value = viewModel.input1.value,
+                            onValueChanged = { viewModel.setInput1(it) },
+                            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+                        )
+
+                        EditNumberField(
+                            label = R.string.second_number,
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Done,
+                            ),
+                            value = viewModel.input2.value,
+                            onValueChanged = { viewModel.setInput2(it) },
+                            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+                        )
+
+                        Button(
+                            onClick = {
+                                viewModel.calculateSum(
+                                    num1 = viewModel.input1.value.toInt(),
+                                    num2 = viewModel.input2.value.toInt(),
+                                )
+                            },
+                        ) {
+                            Text(text = "Add")
+                        }
+                        Text(
+                            text = stringResource(R.string.calculate_sum, viewModel.sum),
+                            style = MaterialTheme.typography.displaySmall,
+                        )
+                        Spacer(modifier = Modifier.height(150.dp))
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun SimpleCalculatorLayout() {
-    var input1 by remember {
-        mutableStateOf("0")
-    }
-    var input2 by remember {
-        mutableStateOf("0")
-    }
-
-    val sum = calculateSum(input1, input2)
-    Column(
-        modifier = Modifier
-            .padding()
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Text(
-            text = stringResource(R.string.calculate_sum),
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .align(alignment = Alignment.Start),
-        )
-
-        EditNumberField(
-            label = R.string.first_number,
-            leadingIcon = R.drawable.numbers,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next,
-            ),
-            value = input1,
-            onValueChanged = { input1 = it },
-            modifier = Modifier
-                .padding(bottom = 32.dp)
-                .fillMaxWidth(),
-        )
-    }
-
-    EditNumberField(
-        label = R.string.second_number,
-        leadingIcon = R.drawable.numbers,
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Number,
-            imeAction = ImeAction.Done,
-        ),
-        value = input2,
-        onValueChanged = { input2 = it },
-        modifier = Modifier
-            .padding(bottom = 32.dp)
-            .fillMaxWidth(),
-    )
-
-    Text(
-        text = stringResource(R.string.calculate_sum, sum),
-        style = MaterialTheme.typography.displaySmall,
-    )
-
-    Spacer(modifier = Modifier.height(150.dp))
-}
-
 /*
- *  This sis a reusable text field
+ *  This is a reusable text field
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
-    @DrawableRes leadingIcon: Int,
+//    @DrawableRes leadingIcon: Int,
     keyboardOptions: KeyboardOptions,
     value: String,
     onValueChanged: (String) -> Unit,
@@ -130,22 +112,10 @@ fun EditNumberField(
     TextField(
         value = value,
         singleLine = true,
-        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
+//        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
         modifier = modifier,
         onValueChange = onValueChanged,
         label = { Text(stringResource(label)) },
         keyboardOptions = keyboardOptions,
     )
-}
-
-private fun calculateSum(num1: String, num2: String): String {
-    return num1 + num2
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SimpleCalculatorPreview() {
-    SimpleCalculatorTheme {
-        SimpleCalculatorLayout()
-    }
 }
--- app/src/main/java/com/example/simplecalculator/MainViewModel.kt
+++ app/src/main/java/com/example/simplecalculator/MainViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.simplecalculator
 
-class MainViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _input1 = mutableStateOf("")
+    val input1: State<String> = _input1
+    fun setInput1(value: String) {
+        _input1.value = value
+    }
+
+    private val _input2 = mutableStateOf("0")
+    val input2: State<String> = _input2
+    fun setInput2(value: String) {
+        _input2.value = value
+    }
+
+    private val _sum = mutableStateOf("0")
+    val sum: State<String> = _sum
+    fun setSum(value: String) {
+        _sum.value = value
+    }
+
+    fun calculateSum(num1: Int, num2: Int) {
+        val sum = num1 + num2
+
+        setSum(sum.toString())
+    }
 }
